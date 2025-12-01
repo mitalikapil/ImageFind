26c108a2528ebc82ca37c2b7283299d3605fc3d2
Mitali Kapil <115822766+mitalikapil@users.noreply.github.com>
2025-12-01 13:32:58 +0530

    Updated Dockerfile for Render

--- server/server.js
+++ server/server.js
@@ -86,7 +86,8 @@ app.get('/api/current_user', (req, res) => {
     }
 });
 
-app.listen(port, "0.0.0.0", () => {
-    console.log(`Server running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
+
 
